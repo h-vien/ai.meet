f8596d456937a3675de8d711ed14f3683050240a
h-vien <vien200203@gmail.com>
2025-05-29 10:04:07 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-green-300">
+      <Button variant="default" size="lg">
+        hello
+      </Button>
+    </div>
+  );
+}
