5dff9eaf64bdf127db15a4a0de71d2f7de2d20fa
h-vien <vien200203@gmail.com>
2025-05-29 10:59:36 +0700

    feat: authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,59 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    console.log("Submitted Values:", email, name, password);
+
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: (error) => {
+          console.error("Error during sign up:", error);
+        },
+        onSuccess: (data) => {
+          console.log("Sign up successful:", data);
+        },
+        onLoading: () => {
+          console.log("Signing up...");
+        },
+      }
+    );
+  };
   return (
     <div className="text-green-300">
-      <Button variant="default" size="lg">
-        hello
+      <Input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button className="mt-4" onClick={onSubmit}>
+        Submit
       </Button>
+      <p className="mt-4">Check the console for the submitted values.</p>
     </div>
   );
 }
